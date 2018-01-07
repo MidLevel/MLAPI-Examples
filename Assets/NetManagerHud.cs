e657cbfcd19bbfb11658d39ad3a3140fa63560d2
Albin Corén <2108U9@gmail.com>
2018-01-07 05:13:10 +0100

    First commit, added project with position sync

---
+++ Assets/NetManagerHud.cs
@@ -0,0 +1,43 @@
+﻿using MLAPI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetManagerHud : MonoBehaviour {
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(20, 20, 100, 20), "Start client"))
+        {
+            NetworkingConfiguration config = new NetworkingConfiguration();
+            config.Address = "127.0.0.1";
+            config.Port = 7777;
+            config.Channels.Add("PositionUpdates", UnityEngine.Networking.QosType.Reliable);
+            config.MessageTypes.Add("PositionUpdate");
+            config.MessageTypes.Add("SetClientPosition");
+            NetworkingManager.singleton.StartClient(config);
+        }
+
+        if (GUI.Button(new Rect(20, 70, 100, 20), "Start server"))
+        {
+            NetworkingConfiguration config = new NetworkingConfiguration();
+            config.Address = "127.0.0.1";
+            config.Port = 7777;
+            config.Channels.Add("PositionUpdates", UnityEngine.Networking.QosType.Reliable);
+            config.MessageTypes.Add("PositionUpdate");
+            config.MessageTypes.Add("SetClientPosition");
+            NetworkingManager.singleton.StartServer(config);
+        }
+
+        if (GUI.Button(new Rect(20, 120, 100, 20), "Start host"))
+        {
+            NetworkingConfiguration config = new NetworkingConfiguration()
+            {
+                Address = "127.0.0.1",
+                Port = 7777
+            };
+            NetworkingManager.singleton.StartHost(config);
+        }
+    }
+}
---
+++ Assets/SyncPosition.cs
@@ -0,0 +1,91 @@
+﻿using MLAPI;
+using System.IO;
+using UnityEngine;
+
+public class SyncPosition : NetworkedBehaviour
+{
+    private float lastSentTime;
+    public float PosUpdatesPerSecond = 20;
+
+
+    private void Awake()
+    {
+        if(isServer)
+        {
+            RegisterMessageHandler("PositionUpdate", OnRecievePositionUpdate);
+        }
+        else
+        {
+            RegisterMessageHandler("SetClientPosition", OnSetClientPosition);
+        }
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+        if(Time.time - lastSentTime > (1f / PosUpdatesPerSecond))
+        {
+            using(MemoryStream stream = new MemoryStream())
+            {
+                using (BinaryWriter writer = new BinaryWriter(stream))
+                {
+                    writer.Write(transform.position.x);
+                    writer.Write(transform.position.y);
+                    writer.Write(transform.position.z);
+                }
+                SendToServer("PositionUpdate", "PositionUpdates", stream.ToArray());
+            }
+            lastSentTime = Time.time;
+        }
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime, Input.GetAxis("Vertical") * Time.deltaTime, 0));
+    }
+
+    //This gets called on all clients except the one the position update is about.
+    void OnSetClientPosition(int connectionId, byte[] data)
+    {
+        using (MemoryStream stream = new MemoryStream(data))
+        {
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                uint networkId = reader.ReadUInt32();
+                if (networkId != objectNetworkId)
+                    return;
+                float x = reader.ReadSingle();
+                float y = reader.ReadSingle();
+                float z = reader.ReadSingle();
+                GetNetworkedObject(networkId).transform.position = new Vector3(x, y, z);
+            }
+        }
+    }
+
+    //This gets called on the server when a client sends it's position.
+    void OnRecievePositionUpdate(int connectionId, byte[] data)
+    {
+        //This makes it behave like a HLAPI Command. It's only invoked on the same object that called it.
+        if (connectionId != ownerConnectionId)
+            return;
+        using (MemoryStream readStream = new MemoryStream(data))
+        {
+            using (BinaryReader reader = new BinaryReader(readStream))
+            {
+                float x = reader.ReadSingle();
+                float y = reader.ReadSingle();
+                float z = reader.ReadSingle();
+                transform.position = new Vector3(x, y, z);
+            }
+            using (MemoryStream writeStream = new MemoryStream())
+            {
+                using (BinaryWriter writer = new BinaryWriter(writeStream))
+                {
+                    writer.Write(objectNetworkId);
+                    writer.Write(transform.position.x);
+                    writer.Write(transform.position.y);
+                    writer.Write(transform.position.z);
+                }
+                //Sends the position to all clients except the one who requested it. Similar to a Rpc with a if(isLocalPlayer) return;
+                SendToNonLocalClients("SetClientPosition", "PositionUpdates", writeStream.ToArray());
+            }
+        }
+    }
+}
