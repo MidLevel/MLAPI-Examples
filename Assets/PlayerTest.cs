54aa28d2b030b97d85dec067ea2b06dbd42bc885
Albin Corén <2108U9@gmail.com>
2018-04-18 15:17:33 +0200

    Added Message sending to example and updated lib to v1.0.1

--- Assets/PlayerTest.cs
+++ Assets/PlayerTest.cs
@@ -1,5 +1,6 @@
 ﻿using MLAPI.Attributes;
 using MLAPI.MonoBehaviours.Core;
+using MLAPI.NetworkingManagerComponents.Binary;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,17 +11,31 @@ public class PlayerTest : NetworkedBehaviour
     public Text TextField;
     public GameObject cubePrefab;
     public GameObject spherePrefab;
+    public Material planeMaterial;
 
     public override void NetworkStart()
     {
-        if (isServer)
+        if (isServer && isLocalPlayer)
             MySyncedName = "SyncVarTest: " + Random.Range(50, 10000);
+
+        planeMaterial = GameObject.Find("Plane").GetComponent<MeshRenderer>().material;
+        if (isClient)
+            RegisterMessageHandler("OnChangeColor", OnChangeColor);
+    }
+
+    private void OnChangeColor(uint clientId, byte[] data)
+    {
+        BitReader reader = new BitReader(data);
+        float r = reader.ReadFloat();
+        float g = reader.ReadFloat();
+        float b = reader.ReadFloat();
+        planeMaterial.color = new Color(r, g, b);
     }
 
     private void OnGUI()
     {
         int y = 25;
-        if (isServer)
+        if (isServer && isLocalPlayer)
         {
             y += 25;
             if (GUI.Button(new Rect(200, y, 200, 20), "Change Text with SyncVar"))
@@ -43,25 +58,25 @@ public class PlayerTest : NetworkedBehaviour
                 go.transform.position = transform.position + new Vector3(0, 3f, 0);
                 go.GetComponent<NetworkedObject>().Spawn();
             }
+
+            y += 25;
+            if (GUI.Button(new Rect(200, y, 200, 20), "Set random plane color"))
+            {
+                planeMaterial.color =  Random.ColorHSV();
+                using (BitWriter writer = new BitWriter())
+                {
+                    writer.WriteFloat(planeMaterial.color.r);
+                    writer.WriteFloat(planeMaterial.color.g);
+                    writer.WriteFloat(planeMaterial.color.b);
+                    SendToClientsTarget("OnChangeColor", "ColorChannel", writer.Finalize());
+                }
+            }
         }
     }
 
     private void Update()
     {
         TextField.text = MySyncedName;
-        if(isServer && Input.GetKeyDown(KeyCode.Space) && Input.GetKeyDown(KeyCode.LeftShift) && isLocalPlayer)
-        {
-            GameObject go = Instantiate(cubePrefab);
-            go.transform.position = transform.position + new Vector3(0, 1f, 0);
-            go.GetComponent<NetworkedObject>().Spawn();
-        }
-        else if(isServer && Input.GetKeyDown(KeyCode.Space) && isLocalPlayer)
-        {
-            MySyncedName = "SyncVarTest: " + Random.Range(50, 10000) + " (Press space on server)";
-            GameObject go = Instantiate(spherePrefab);
-            go.transform.position = transform.position + new Vector3(0, 1f, 0);
-            go.GetComponent<NetworkedObject>().Spawn();
-        }
     }
 
 }
